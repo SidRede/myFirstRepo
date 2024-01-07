2cf24434ae1940ba4cd235710c6c8ab914bbad58
SidRede <siddheshwar.rede21@pccoepune.org>
2024-01-07 19:03:00 +0530

    New book images and coroudel added successfully

--- index.php
+++ index.php
@@ -12,11 +12,68 @@
     <style>
         .w-100 {
           margin-top: 3vh;
-    width: 66%!important;
-    margin-left: 16vw;
-    height: 55vh;
+          width: 66%!important;
+          margin-left: 16vw;
+          height: 55vh;
         }
+    
+        .carousal-container {
+            overflow: hidden;
+            width: 100%;
+            position: relative;
+        }
+
+        .carousal {
+            display: flex;
+            transition: transform 0.5s ease-in-out;
+        }
+
+        .card {
+          height: 52vh;
+            overflow:hidden;
+            flex: 0 0 auto;
+            width: 300px; /* Set your desired width */
+            margin: 0 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+            overflow: hidden;
+            transition: transform 0.5s ease-in-out;
+        }
+
+        .card > img {
+          margin-top: 20px;
+          margin-left: 20px;
+          height: 245px;
+          width: 254px;
+        }
+
+      .card-content{
+        padding : 0px 20px 0px 20px;
+        margin-top:20px;
+        display :flex;
+        justify-content : space-around;
+        /* align-self : center: */
+      }
+        
+        .prev, .next {
+            cursor: pointer;
+            font-size: 24px;
+            user-select: none;
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+        }
+
+        .prev {
+            left: 10px;
+        }
+
+        .next {
+            right: 10px;
+        }
+    
     </style>
+
 </head>
 
 
@@ -42,13 +99,13 @@
     <section class="hero">
     <div class="image">
             <!-- corousel  -->
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
             
       <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="2500">
-          <img src="./images/bookshelve1.jpeg" class="d-block w-100" alt="...">
-        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="2500">
+              <img src="./images/bookshelve1.jpeg" class="d-block w-100" alt="...">
+            </div>
         <div class="carousel-item" data-bs-interval="2500">
           <img src="./images/bookshelve2.jpeg" class="d-block w-100" alt="...">
         </div>
@@ -56,15 +113,24 @@
           <img src="./images/bookshelve3.webp" class="d-block w-100" alt="...">
         </div>
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+      
+      <button class="carousel-control-prev" type="button"             data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+        
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
       </button>
+
       <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
       </button>
     </div>
+
+
+
+
+
+
 
         <div class="lineDiv">
           <div class="line1 line">
@@ -79,64 +145,171 @@
 
         
         <div class="booksOnSell">
-            <div class="card" style="width: 18rem;">
-                <img src="./images/harryPotter.webp" class="card-img-top" alt="book1">
-                <div class="card-body">
-                  <div class="bookInfo">
-                    <h5 class="card-title">Harry Potter</h5>
-                    <h6>Rs 500</h2>
-                  </div>
-                  <!-- <p class="card-text"></p> -->
-                  <div class="buttonBuyNow">
+            
+        <div class="carousal-container">
+          <div class="carousal">
+        
+                <div class="card">
+                    <img src="./images/gaurGopal.jpg" alt="Image 1">
+                    <div class="card-content">
+                      <div class="bookInfo">
+                        <h5 class = "card-title">Energize your Mind </h5>
+                          <h6>Rs 180</h6>
+                      </div>
+                        
+                        <div class="buttonBuyNow">
                     <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
                   </div>
-
-                  
+                    </div>
                 </div>
-              </div>
 
-              <div class="card" style="width: 18rem;">
-                <img src="./images/twelthFail.webp" class="card-img-top" alt="book1">
-                <div class="card-body">
-                  <div class="bookInfo">
-                    <h5 class="card-title">12th Fail</h5>
-                    <h6>Rs 180</h2>
-                  </div>
-                  <!-- <p class="card-text"></p> -->
-                  <div class="buttonBuyNow">
+                <div class="card">
+                    <img src="./images/harryPotter.webp" alt="Image 1">
+                    <div class="card-content">
+                      <div class="bookInfo">
+                        <h5 class = "card-title">Harry Potter Series </h5>
+                          <h6>Rs 2000</h6>
+                      </div>
+                        
+                        <div class="buttonBuyNow">
                     <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
                   </div>
+                    </div>
                 </div>
-              </div>
 
-              <div class="card" style="width: 18rem;">
-                <img src="./images/gaurGopal.jpg" class="card-img-top" alt="book1">
-                <div class="card-body">
-                  <div class="bookInfo">
-                    <h5 class="card-title">Energize Your Mind</h5>
-                    <h6>Rs 250</h2>
-                  </div>
-                  <!-- <p class="card-text"></p> -->
-                  <div class="buttonBuyNow">
+                <div class="card">
+                    <img src="./images/twelthFail.webp" alt="Image 1">
+                    <div class="card-content">
+                      <div class="bookInfo">
+                        <h5 class = "card-title">12th Fail </h5>
+                          <h6>Rs 200</h6>
+                      </div>
+                        
+                        <div class="buttonBuyNow">
                     <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
                   </div>
+                    </div>
                 </div>
-              </div>
 
-              <div class="card" style="width: 18rem;">
-                <img src="./images/vishwaas.jfif" class="card-img-top" alt="book1">
-                <div class="card-body">
-                  <div class="bookInfo">
-                    <h5 class="card-title">Mn mai hai Vishwaas</h5>
-                    <h6>Rs 200</h2>
-                  </div>
-                  <!-- <p class="card-text"></p> -->
-                  <div class="buttonBuyNow">
+                <div class="card">
+                    <img src="./images/vishwaas.jfif" alt="Image 1">
+                    <div class="card-content">
+                      <div class="bookInfo">
+                        <h5 class = "card-title">Mn mai hai Vishwaas </h5>
+                          <h6>Rs 180</h6>
+                      </div>
+                        
+                        <div class="buttonBuyNow">
                     <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
                   </div>
+                    </div>
                 </div>
-              </div>
 
+                <div class="card">
+                    <img src="./images/Netaji.jpg" alt="Image 1">
+                    <div class="card-content">
+                      <div class="bookInfo">
+                        <h5 class = "card-title">What Happened To Netaji ? </h5>
+                          <h6>Rs 200</h6>
+                      </div>
+                        
+                        <div class="buttonBuyNow">
+                    <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
+                  </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="./images/IndianUprising.jpg" alt="Image 1">
+                    <div class="card-content">
+                      <div class="bookInfo">
+                        <h5 class = "card-title">The Great Indian Uprising </h5>
+                          <h6>Rs 200</h6>
+                      </div>
+                        
+                        <div class="buttonBuyNow">
+                    <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
+                  </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="./images/gitanjaliBook.jpg" alt="Image 1">
+                    <div class="card-content">
+                      <div class="bookInfo">
+                        <h5 class = "card-title">Gitanjali </h5>
+                          <h6>Rs 300</h6>
+                      </div>
+                        
+                        <div class="buttonBuyNow">
+                    <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
+                  </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="./images/godanBook.jpg" alt="Image 1">
+                    <div class="card-content">
+                      <div class="bookInfo">
+                        <h5 class = "card-title">Godaan </h5>
+                          <h6>Rs 200</h6>
+                      </div>
+                        
+                        <div class="buttonBuyNow">
+                    <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
+                  </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="./images/dopehriBook.jfif" alt="Image 1">
+                    <div class="card-content">
+                      <div class="bookInfo">
+                        <h5 class = "card-title">Dopehari </h5>
+                          <h6>Rs 200</h6>
+                      </div>
+                        
+                        <div class="buttonBuyNow">
+                    <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
+                  </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="./images/ravaanBook.jpg" alt="Image 1">
+                    <div class="card-content">
+                      <div class="bookInfo">
+                        <h5 class = "card-title">Ravaan </h5>
+                          <h6>Rs 250</h6>
+                      </div>
+                        
+                        <div class="buttonBuyNow">
+                    <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
+                  </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="./images/loveBook11.jfif" alt="Image 1">
+                    <div class="card-content">
+                      <div class="bookInfo">
+                        <h5 class = "card-title">Love in Ancient India </h5>
+                          <h6>Rs 200</h6>
+                      </div>
+                        
+                        <div class="buttonBuyNow">
+                    <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
+                  </div>
+                    </div>
+                </div>
+        
+          </div>
+          <div class="prev">&#10094;</div>
+          <div class="next">&#10095;</div>
+        </div>
+            
+
+            
         </div>
 
         <div class="lineDiv">
@@ -150,37 +323,35 @@
         </div>
 
         <div class="booksOnSell">
-            <div class="card" style="width: 18rem;">
-                <img src="./images/harryPotter.webp" class="card-img-top" alt="book1">
-                <div class="card-body">
-                  <div class="bookInfo">
-                    <h5 class="card-title">Harry Potter</h5>
-                    <h6>Rs 500</h2>
-                  </div>
-                  <!-- <p class="card-text"></p> -->
-                  <div class="buttonBuyNow">
+        <div class="card">
+                    <img src="./images/gaurGopal.jpg" alt="Image 1">
+                    <div class="card-content">
+                      <div class="bookInfo">
+                        <h5 class = "card-title">Energize your Mind </h5>
+                          <h6>Rs 180</h6>
+                      </div>
+                        
+                        <div class="buttonBuyNow">
                     <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
                   </div>
-
-                  
+                    </div>
                 </div>
-              </div>
 
-              <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 18rem;">
                 <img src="./images/twelthFail.webp" class="card-img-top" alt="book1">
                 <div class="card-body">
                   <div class="bookInfo">
                     <h5 class="card-title">12th Fail</h5>
-                    <h6>Rs 180</h2>
+                    <h6>Rs 180</h6>
                   </div>
                   <!-- <p class="card-text"></p> -->
                   <div class="buttonBuyNow">
                     <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
                   </div>
                 </div>
-              </div>
+            </div>
 
-              <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 18rem;">
                 <img src="./images/gaurGopal.jpg" class="card-img-top" alt="book1">
                 <div class="card-body">
                   <div class="bookInfo">
@@ -192,9 +363,9 @@
                     <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
                   </div>
                 </div>
-              </div>
+            </div>
 
-              <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 18rem;">
                 <img src="./images/vishwaas.jfif" class="card-img-top" alt="book1">
                 <div class="card-body">
                   <div class="bookInfo">
@@ -206,7 +377,7 @@
                     <a href="#" class="btn btn-primary"><center> Buy Now  </center></a>
                   </div>
                 </div>
-              </div>
+            </div>
 
         </div>
 
@@ -286,8 +457,58 @@
 
     </section>
 
+    <script>
+          const carousal = document.querySelector('.carousal');
+          const cards = document.querySelectorAll('.card');
+          const cardWidth = cards[0].offsetWidth;
+          const cardsInView = 6;
+          let currentIndex = 0;
 
-   
+          // Clone cards for seamless wrapping
+          for (let i = 0; i < cardsInView; i++) {
+            const clone = cards[i].cloneNode(true);
+            carousal.appendChild(clone);
+          }
+
+          function updateCarousal() {
+            const newTransformValue = -currentIndex * cardWidth + 'px';
+            carousal.style.transition = 'transform 0.5s ease-in-out';
+            carousal.style.transform = 'translateX(' + newTransformValue + ')';
+          }
+
+          function nextSlide() {
+            currentIndex++;
+            updateCarousal();
+            if (currentIndex === cards.length - cardsInView + 1) {
+              setTimeout(() => {
+                currentIndex = 0;
+                carousal.style.transition = 'none';
+                updateCarousal();
+              }, 500);
+            }
+          }
+
+          function prevSlide() {
+            currentIndex--;
+            updateCarousal();
+            if (currentIndex < 0) {
+              setTimeout(() => {
+                currentIndex = cards.length - cardsInView;
+                carousal.style.transition = 'none';
+                updateCarousal();
+              }, 500);
+            }
+          }
+
+          const nextBtn = document.querySelector('.next');
+          const prevBtn = document.querySelector('.prev');
+
+          nextBtn.addEventListener('click', nextSlide);
+          prevBtn.addEventListener('click', prevSlide);
+
+          // Set up automatic sliding every 3 seconds (adjust as needed)
+          setInterval(nextSlide, 3000);
+    </script>
 
 
 
@@ -332,6 +553,14 @@
 
 
     </footer>
+
+
+   
+  
+
+
+
+
 </body>
 </html>
 
